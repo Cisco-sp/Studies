5eed0dddc02ea0b3017fec1765d301b85f166aab
Henrique Prado <lipnose1@gmail.com>
2020-01-31 21:29:33 -0300

    função open_json

--- heprado/functions.py
+++ heprado/functions.py
@@ -2,9 +2,15 @@ import json
 
 json_inventario="C:\Users\heprado\Documents\Github\Cisco\Studies\heprado\inventario.json"
 
-def open_json(json):
+def open_json(arquivo):
 
-    with open(json,'+') as 
+    with open(arquivo,'+') as arquivo_aberto:
+        
+        parseado=json.dumps(arquivo_aberto)
+        
+return parseado
+
+
         
 
 
--- heprado/inventario.py
+++ heprado/inventario.py
@@ -1,1 +1,5 @@
+import functions.py
+
+def open_json()
+
 json_inventario="C:\Users\heprado\Documents\Github\Cisco\Studies\heprado\inventario.json"
